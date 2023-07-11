6858399e15a4c9bc9c6c13fdb055df9b33efc820
zyz1059 <1059260994@qq.com>
2023-07-11 23:06:37 +0800

    update

--- ReadMe.m
+++ ReadMe.m
@@ -2,6 +2,6 @@
 % 1. 运行参数初始化文件 init_simulation.m
 % 2. 运行仿真程序 HeliControl_with_UDE.slx(R2022a) 或旧版本程序HeliControl_with_UDE_R2018a.slx/mdl
 % 结果展示：
-% 1. result_plot：UDE参数=10,0.1和无UDE三种条件下的仿真结果图
-% 2. result_video：UDE参数=10,1和无UDE三种条件下的实物实验视频
+% 1. result_plot：不同UDE参数条件下的仿真结果图
+% 2. result_video：不同UDE参数条件下的实物实验视频（包含激光打靶视频）
 
